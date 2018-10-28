2b32978bd95af00c40628ba28233912236297eb4
AntonyGlim <Antony.Glim@gmail.com>
2018-10-28 17:18:32 +0300

    регулярные все в одну строку

--- src/lesson_3_Collections_and_Regular_expressions/regularExpressionsTask/RegularExpressionsTask.java
+++ src/lesson_3_Collections_and_Regular_expressions/regularExpressionsTask/RegularExpressionsTask.java
@@ -12,12 +12,14 @@
 package lesson_3_Collections_and_Regular_expressions.regularExpressionsTask;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegularExpressionsTask {
 
     public static void main(String[] args) {
         System.out.println("Введите ваш вариант пароля:");
-        boolean isValid = userEntersPasword();
+        System.out.println(userEntersPassword());
     }
 
     /**
@@ -25,16 +27,9 @@ public class RegularExpressionsTask {
      * пока он не получит валидное значение
      * @return целочисленный порядковый номер дня введенный пользователем
      */
-    public static boolean userEntersPasword() {
-        String dayWhichUserIn;                                                              //Строка, которую ввел пользователь
+    public static boolean userEntersPassword() {
         Scanner in = new Scanner(System.in);
-
-        do {                                                                                //Защита от не корректных данных
-            dayWhichUserIn = in.nextLine();                                                 //Принимаем значение пользователя
-        } while (!isValid(dayWhichUserIn));                                                 //До тех пор, пока не получим валидные значения
-
-        int result = Integer.parseInt(dayWhichUserIn);                                          //Преобразовываем введенное пользователем в число
-        return result;
+        return isValid(in.nextLine());
     }
 
     /**
@@ -43,19 +38,8 @@ public class RegularExpressionsTask {
      * @return соответствие строки требованиям
      */
     private static boolean isValid(String str){
-        boolean isTrue = true;                                                                      //Изначально соответствует требованиям
-        int dayWhichUserIn;
-        if (!str.matches("-?\\d+")){                                                            //Если не целое число (отрицательные тоже распознаются)
-            System.err.println("Значение должно быть числом!");
-            isTrue = false;
-        } else {
-            dayWhichUserIn = Integer.parseInt(str);                                                 //Преобразовываем введенное пользователем в число
-            if (dayWhichUserIn < 1 || dayWhichUserIn > 7) {                                         //Если число не попадает в указанный диапазон
-                System.err.println("Такого дня недели нет! Попробуйте еще раз!");
-                isTrue = false;
-            }
-        }
-        return isTrue;
+        Pattern p = Pattern.compile("^[a-zA-Z0-9]{8}+$");
+        Matcher m = p.matcher(str);
+        return m.matches();
     }
-}
 }
