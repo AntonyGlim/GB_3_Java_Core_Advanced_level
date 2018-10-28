05455f121a5c08c57f44bb89d4770181363dc3f4
AntonyGlim <Antony.Glim@gmail.com>
2018-10-28 18:40:00 +0300

    Реорганизована структура

--- src/lesson_3_Collections_and_Regular_expressions/collectionsTask/CollectionsTaskPhonebook.java
+++ src/lesson_3_Collections_and_Regular_expressions/collectionsTask/CollectionsTaskPhonebook.java
@@ -1,4 +1,27 @@
 package lesson_3_Collections_and_Regular_expressions.collectionsTask;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CollectionsTaskPhonebook {
+    public static void main(String[] args) {
+        Map<String, String> phoneBook = new HashMap<>();
+
+
+        phoneBook.put("3-32-76", "Иванов");
+        phoneBook.put("3-32-79", "Петров");
+        phoneBook.put("3-32-12", "Сидоров");
+        phoneBook.put("3-32-02", "Ким");
+        phoneBook.put("3-32-17", "Заболотный");
+        phoneBook.put("3-32-84", "Сидоров");
+        phoneBook.put("3-32-25", "Иванов");
+        phoneBook.put("3-32-03", "Иванов");
+
+
+        for (Map.Entry<String, String> o : phoneBook.entrySet()){
+            if (o.getValue() == "Иванов"){
+                System.out.print("По запросу " + o.getValue() + "Найдено: " + o.getKey());
+            }
+        }
+    }
 }
--- src/lesson_3_Collections_and_Regular_expressions/collectionsTask/CollectionsTaskWorkWithArray.java
+++ src/lesson_3_Collections_and_Regular_expressions/collectionsTask/CollectionsTaskWorkWithArray.java
@@ -1,4 +1,43 @@
+/**
+ *  1. Создать массив с набором слов (10-20 слов, среди которых должны встречаться повторяющиеся).
+ *  Найти и вывести список уникальных слов, из которых состоит массив (дубликаты не считаем).
+ *  Посчитать, сколько раз встречается каждое слово.
+ *
+ */
 package lesson_3_Collections_and_Regular_expressions.collectionsTask;
 
+import java.util.*;
+
 public class CollectionsTaskWorkWithArray {
+    public static void main(String[] args) {
+
+        ArrayList<String> arrayList = new ArrayList<>(); 
+        arrayList.add("азот");  arrayList.add("бинт");  arrayList.add("воин");
+        arrayList.add("грот");  arrayList.add("дюйм");  arrayList.add("жанр");
+        arrayList.add("кожа");  arrayList.add("кран");  arrayList.add("бинт");
+        arrayList.add("грот");  arrayList.add("воин");  arrayList.add("воин");
+        arrayList.add("воин");  arrayList.add("бинт");  arrayList.add("азот");
+
+        System.out.println("Уникальные значения: ");
+        uniqueWordsListInfo(arrayList);
+        System.out.println("\nСколько раз встречается слово в листе: ");
+        repeatedWordsNumberInfo(arrayList);
+    }
+
+    public static void uniqueWordsListInfo(ArrayList<String> arrayList){
+        Set<String> setList = new HashSet();
+        setList.addAll(arrayList);
+        for (String s : setList){
+            System.out.println(s);
+        }
+    }
+
+    public static void repeatedWordsNumberInfo(ArrayList<String> arrayList){
+        Map<String, Integer> mapList = new HashMap<>();
+        for (String a : arrayList){
+            Integer number = mapList.get(a);
+            mapList.put(a, number == null ? 1 : number + 1);
+        }
+        System.out.println(mapList);
+    }
 }
