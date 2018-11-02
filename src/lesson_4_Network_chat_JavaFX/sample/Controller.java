69779e3a9563111c59140e4cee9e870c74a6eec2
AntonyGlim <Antony.Glim@gmail.com>
2018-11-02 23:20:11 +0300

    Все работает корявее некуда

--- src/lesson_4_Network_chat_JavaFX/sample/Controller.java
+++ src/lesson_4_Network_chat_JavaFX/sample/Controller.java
@@ -3,10 +3,11 @@ package lesson_4_Network_chat_JavaFX.sample;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
 import javax.swing.plaf.basic.BasicSplitPaneUI;
 
@@ -24,8 +25,28 @@ public class Controller {
     @FXML
     Button buttonExit;
 
+    @FXML
+    VBox vBoxChatPlace;
+
+    boolean rightAlignment = false;
     public void sendMsg(ActionEvent actionEvent) {
-        textArea.appendText(textField.getText() + "\n");
+        Label label = new Label();
+        label.setText(textField.getText() + "\n");
+        if (rightAlignment){
+            vBoxChatPlace.setAlignment(Pos.BOTTOM_RIGHT);
+            vBoxChatPlace.getChildren().add(label);
+            rightAlignment = false;
+        } else {
+            vBoxChatPlace.setAlignment(Pos.BOTTOM_LEFT);
+            vBoxChatPlace.getChildren().add(label);
+            rightAlignment = true;
+        }
+
+
+//        vBoxChatPlace.getChildren().add(label);
+//        vBoxChatPlace.setAlignment(Pos.BOTTOM_LEFT);
+
+//        textArea.appendText(textField.getText() + "\n");
         textField.clear();
         textField.requestFocus();
     }
