a44b4bc034bce2221c80f21fb28d24d90224663a
AntonyGlim <Antony.Glim@gmail.com>
2018-11-09 12:12:53 +0300

    Написан ClientIn

--- src/lesson_6_Work_with_network/consoleСhatTask/client/ClientIn.java
+++ src/lesson_6_Work_with_network/consoleСhatTask/client/ClientIn.java
@@ -1,9 +1,38 @@
 package lesson_6_Work_with_network.consoleСhatTask.client;
 
-public class ClientIn  implements Runnable{
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class ClientIn implements Runnable{
+
+    private Socket socket;
+    private DataInputStream in;
+
+
+    public ClientIn(Socket socket) throws IOException {
+        this.socket = socket;
+        in = new DataInputStream(socket.getInputStream());
+    }
 
     @Override
     public void run() {
-
+        while (!socket.isClosed()){
+            try {
+                System.out.println(in.readUTF());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    in.close();
+                    socket.close();
+                    System.out.println("Соединение прервано");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- src/lesson_6_Work_with_network/consoleСhatTask/server/ServerIn.java
+++ src/lesson_6_Work_with_network/consoleСhatTask/server/ServerIn.java
@@ -1,6 +1,11 @@
 package lesson_6_Work_with_network.consoleСhatTask.server;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class ServerIn  implements Runnable{
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     @Override
     public void run() {
