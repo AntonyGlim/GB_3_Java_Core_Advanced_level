2cacd2e40e0df6842709c4af577de5853fec5663
AntonyGlim <Antony.Glim@gmail.com>
2018-11-09 16:41:44 +0300

    Прописаны сервер старт и клиент старт

--- src/lesson_6_Work_with_network/consoleСhatTask/server/ServerStart.java
+++ src/lesson_6_Work_with_network/consoleСhatTask/server/ServerStart.java
@@ -1,5 +1,12 @@
 package lesson_6_Work_with_network.consoleСhatTask.server;
 
+
+import java.io.IOException;
+
 public class ServerStart {
+    public static void main(String[] args) {
+
+            new ServerStart();
+    }
 
 }
