b848507b9494f3ba1f79a9d8d70f0cd03103911d
AntonyGlim <Antony.Glim@gmail.com>
2018-11-11 20:46:07 +0300

    Почитать в Метро

---
+++ src/lesson_7_NetworkChat_part_1/methodicalInstruction/MainClient/MainClass.java
@@ -0,0 +1,7 @@
+package lesson_7_NetworkChat_part_1.methodicalInstruction.MainClient;
+
+public class MainClass {
+    public static void main(String[] args) {
+        new MyWindow();
+    }
+}
---
+++ src/lesson_7_NetworkChat_part_1/methodicalInstruction/MainServer/MainClass.java
@@ -0,0 +1,9 @@
+package lesson_7_NetworkChat_part_1.methodicalInstruction.MainServer;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+        new MyServer();
+
+    }
+}
