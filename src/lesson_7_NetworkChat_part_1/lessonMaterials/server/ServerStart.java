0507a7b784b7e7d2f44dc4e0f95b06203c31e8a9
AntonyGlim <Antony.Glim@gmail.com>
2018-11-12 23:31:41 +0300

    Удалено лишнее

--- src/lesson_7_NetworkChat_part_1/lessonMaterials/server/ServerStart.java
+++
@@ -1,9 +0,0 @@
-package lesson_7_NetworkChat_part_1.lessonMaterials.server;
-
-import java.sql.SQLException;
-
-public class ServerStart {
-    public static void main(String[] args) throws SQLException {
-        new MainServer();
-    }
-}
