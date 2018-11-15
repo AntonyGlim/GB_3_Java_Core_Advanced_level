4ef59730b61ac2d1e0feff4d20b1f02458f740fe
AntonyGlim <Antony.Glim@gmail.com>
2018-11-15 21:00:54 +0300

    Добавлены комментарии в ClientHandler

--- src/lesson_8_NetworkChat_part_2/Server/ClientHandler.java
+++ src/lesson_8_NetworkChat_part_2/Server/ClientHandler.java
@@ -1,3 +1,8 @@
+/**
+ * Обработчик клиента
+ * В классе описана авторизация пользователя со стороны сервера,
+ * а так же работа с клиентом
+ */
 package lesson_8_NetworkChat_part_2.Server;
 
 import java.io.DataInputStream;
@@ -28,53 +33,17 @@ public class ClientHandler {
             this.out = new DataOutputStream(socket.getOutputStream());
             this.blackList = new ArrayList<>();
 
-            new Thread(new Runnable() {
+            new Thread(new Runnable() {                                     //Работаем с каждым клиентом в отдельном потоке
                 @Override
                 public void run() {
                     try {
-                        while (true) {
-                            String str = in.readUTF();
-                            if(str.startsWith("/auth")) {
-                                String[] tokens = str.split(" ");
-                                String newNick = AuthService.getNickByLoginAndPass(tokens[1], tokens[2]);
-                                if(newNick != null) {
-                                    if(!server.isNickBusy(newNick)) {
-                                        sendMsg("/authok");
-                                        nick = newNick;
-                                        server.subscribe(ClientHandler.this);
-                                        break;
-                                    } else {
-                                        sendMsg("Учетная запись уже используется!");
-                                    }
-                                } else {
-                                    sendMsg("Неверный логин/пароль!");
-                                }
-                            }
-                        }
 
-                        while (true) {
-                            String str = in.readUTF();
-                            if(str.startsWith("/")) {
-                                if(str.equals("/end")) {
-                                    out.writeUTF("/serverClosed");
-                                }
-                                if(str.startsWith("/w ")) {
-                                    String[] tokens = str.split(" ",3);
-                                    server.sendPersonalMsg(ClientHandler.this, tokens[1], tokens[2]);
-                                }
-                                if(str.startsWith("/blacklist ")) {
-                                    String[] tokens = str.split(" ");
-                                    blackList.add(tokens[1]);
-                                    sendMsg("Вы добавили пользователя " + tokens[1] + " в черный список");
-                                }
-                            } else {
-                                server.broadcastMsg(ClientHandler.this,nick + ": " + str);
-                            }
-                        }
+                        clientAuthorization();                              //Авторизация клиента
+                        clientWork();                                       //Работа с клиентом
 
                     } catch (IOException e) {
                         e.printStackTrace();
-                    } finally {
+                    } finally {                                             //Закрываем все потоки и сокет
                         try {
                             in.close();
                         } catch (IOException e) {
@@ -90,7 +59,7 @@ public class ClientHandler {
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
-                        server.unsubscribe(ClientHandler.this);
+                        server.unsubscribe(ClientHandler.this);     //Убираем клиента из списка подключенных
                     }
                 }
             }).start();
@@ -110,7 +79,7 @@ public class ClientHandler {
     }
 
     /**
-     * Метод посылает сообщение конкретному пользователю
+     * Метод посылает сообщение одному пользователю
      * @param msg - сообщение
      */
     public void sendMsg(String msg) {
@@ -120,4 +89,53 @@ public class ClientHandler {
             e.printStackTrace();
         }
     }
+
+    /**
+     * Авторизация клиента и проверка корректности введенного логина и пароля
+     * @throws IOException
+     */
+    public void clientAuthorization() throws IOException {
+        while (true) {
+            String str = in.readUTF();
+            if(str.startsWith("/auth")) {
+                String[] tokens = str.split(" ");
+                String newNick = AuthService.getNickByLoginAndPass(tokens[1], tokens[2]);
+                if(newNick != null) {
+                    if(!server.isNickBusy(newNick)) {
+                        sendMsg("/authok");
+                        nick = newNick;
+                        server.subscribe(ClientHandler.this);
+                        break;
+                    } else {
+                        sendMsg("Учетная запись уже используется!");
+                    }
+                } else {
+                    sendMsg("Неверный логин/пароль!");
+                }
+            }
+        }
+    }
+
+    public void clientWork () throws IOException {
+        while (true) {
+            String str = in.readUTF();
+            if(str.startsWith("/")) {
+                if(str.equals("/end")) {
+                    out.writeUTF("/serverClosed");
+                }
+                if(str.startsWith("/w ")) {
+                    String[] tokens = str.split(" ",3);
+                    server.sendPersonalMsg(ClientHandler.this, tokens[1], tokens[2]);
+                }
+                if(str.startsWith("/blacklist ")) {
+                    String[] tokens = str.split(" ");
+                    blackList.add(tokens[1]);
+                    sendMsg("Вы добавили пользователя " + tokens[1] + " в черный список");
+                }
+            } else {
+                server.broadcastMsg(ClientHandler.this,nick + ": " + str);
+            }
+        }
+    }
+
 }
